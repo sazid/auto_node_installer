f9723f15314752e698bfe62b736f3cd9015dfd7f
sazid <sazidozon@gmail.com>
2022-05-26 16:46:41 +0600

    try py file association while installing

--- internal/zeuz_node/python/install_windows.go
+++ internal/zeuz_node/python/install_windows.go
@@ -13,10 +13,17 @@ import (
 func installPython(pythonInstallerPath, pythonInstallDir string) {
 	log.Println("installing python")
 
-	installerArgs := []string{
+	tryInstall := func(args []string) error {
+		cmd := exec.Command(pythonInstallerPath, args...)
+
+		err := cmd.Run()
+		return err
+	}
+
+	if err := tryInstall([]string{
 		"/passive",
 		"InstallAllUsers=0",
-		"Include_launcher=0",
+		"Include_launcher=1",
 		"Include_test=0",
 		"Include_pip=1",
 		"Include_tools=1",
@@ -24,13 +31,26 @@ func installPython(pythonInstallerPath, pythonInstallDir string) {
 		"Include_dev=1",
 		"Include_tcltk=1",
 		"PrependPath=1",
-		"AssociateFiles=1",
+		"AssociateFiles=0",
 		fmt.Sprintf("DefaultJustForMeTargetDir=%s", pythonInstallDir),
-	}
-	cmd := exec.Command(pythonInstallerPath, installerArgs...)
-
-	err := cmd.Run()
-	if err != nil {
-		log.Fatalf("failed to install python: %v", err)
+	}); err != nil {
+		log.Println("failed to obtain admin rights, installing without .py file association")
+		log.Printf("to associate .py files, right click on any .py file, select Open With and select 'python.exe' from this folder: %s", pythonInstallDir)
+		if err := tryInstall([]string{
+			"/passive",
+			"InstallAllUsers=0",
+			"Include_launcher=0",
+			"Include_test=0",
+			"Include_pip=1",
+			"Include_tools=1",
+			"Include_exe=1",
+			"Include_dev=1",
+			"Include_tcltk=1",
+			"PrependPath=1",
+			"AssociateFiles=1",
+			fmt.Sprintf("DefaultJustForMeTargetDir=%s", pythonInstallDir),
+		}); err != nil {
+			log.Fatalf("failed to install python: %v", err)
+		}
 	}
 }
